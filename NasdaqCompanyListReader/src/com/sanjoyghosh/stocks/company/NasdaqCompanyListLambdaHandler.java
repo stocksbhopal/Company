2376d77aa56cfc277baaeac9bd69d3e432170a47
stocksbhopal <stocksbhopal@gmail.com>
2015-10-18 11:45:58 -0700

    Add initial checkin

---
+++ NasdaqCompanyListReader/src/com/sanjoyghosh/stocks/company/NasdaqCompanyListLambdaHandler.java
@@ -0,0 +1,51 @@
+package com.sanjoyghosh.stocks.company;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.sanjoyghosh.stocks.library.db.JPAHelper;
+
+public class NasdaqCompanyListLambdaHandler implements RequestHandler<S3Event, Object> {
+
+    @Override
+    public Object handleRequest(S3Event s3event, Context context) {
+        context.getLogger().log("Input: " + "ENTER THE DRAGON AGAIN");
+    	JPAHelper.createEntityManager();
+    	return null;
+    	
+    	/*
+        S3EventNotificationRecord s3Record = s3event.getRecords().get(0);
+        String s3Bucket = s3Record.getS3().getBucket().getName();
+        String s3Key = s3Record.getS3().getObject().getKey();
+        
+        S3Object s3Object = s3Client.getObject(s3Bucket, s3Key);
+        Reader s3Reader = null;
+        try {
+        	s3Reader = new InputStreamReader(s3Object.getObjectContent(), "UTF-8");
+        	NasdaqCompanyListReader companyListReader = new NasdaqCompanyListReader();
+        	companyListReader.readCompanyList(s3Reader);
+        } 
+        catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} 
+        catch (IOException e) {
+			e.printStackTrace();
+		}
+        finally {
+        	if (s3Reader != null) {
+        		try {
+        			s3Reader.close();
+				} 
+        		catch (IOException e) {
+					e.printStackTrace();
+				}
+        	}
+        }
+
+        context.getLogger().log("Input: " + s3Bucket + "  " + s3Key);
+        // TODO: implement your handler
+        return null;
+        */
+    }
+
+}
