9f505caabd44198b5d66f97c27d77898ff5e84f1
Sanjoy Ghosh <sanjoyg@lab126.com>
2016-11-19 15:10:36 -0800

    Fixes 11/19/2016

---
+++ EarningsLambda/src/com/sanjoyghosh/company/lambda/EarningsSpeechlet.java
@@ -0,0 +1,39 @@
+package com.sanjoyghosh.company.lambda;
+
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+
+public class EarningsSpeechlet implements Speechlet {
+
+	@Override
+	public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
+		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+		outputSpeech.setText("You will have great earnings soon");
+		return SpeechletResponse.newTellResponse(outputSpeech);
+	}
+
+	@Override
+	public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
+		// TODO Auto-generated method stub
+		
+	}
+}
