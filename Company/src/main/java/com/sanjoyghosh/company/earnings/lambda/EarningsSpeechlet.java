3892b251e7d8bfb32c823fb997c9542c298421f7
Sanjoy Ghosh <sghosh@hytrust.com>
2017-02-08 11:04:53 -0800

    Fixes 2/8/2017

--- Company/src/main/java/com/sanjoyghosh/company/earnings/lambda/EarningsSpeechlet.java
+++ Company/src/main/java/com/sanjoyghosh/company/earnings/lambda/EarningsSpeechlet.java
@@ -3,9 +3,6 @@ package com.sanjoyghosh.company.earnings.lambda;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
@@ -26,8 +23,6 @@ import com.sanjoyghosh.company.earnings.utils.CompanyFactsUtils;
 
 public class EarningsSpeechlet implements Speechlet  {
 
-    private static final Logger log = LoggerFactory.getLogger(EarningsSpeechlet.class);
-    
     private static final Map<String, InterfaceIntent> intentInterfaceByIntentNameMap = new HashMap<>();
     static {
     	CompanyFacts cf = CompanyFactsUtils.getCompanyFactsForSymbol("AMZN");
@@ -57,13 +52,12 @@ public class EarningsSpeechlet implements Speechlet  {
 	
     @Override
 	public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
-		// TODO Auto-generated method stub
-	}
+    	System.out.println("onSessionStarted: " + session.getSessionId() + "  " + session.getAttributes().size());
+    }
 
 
 	@Override
 	public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -71,7 +65,7 @@ public class EarningsSpeechlet implements Speechlet  {
 	@Override
 	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
 		String intentName = request.getIntent().getName();
-		log.info("Intent: " + intentName);
+		System.out.println("Intent: " + intentName);
 
 		if (intentName.equals("AMAZON.YesIntent")) {
 			String lastIntentName = (String) session.getAttribute(InterfaceIntent.ATTR_LAST_INTENT);
@@ -84,7 +78,7 @@ public class EarningsSpeechlet implements Speechlet  {
 			return interfaceIntent.onIntent(request, session);
 		}
 
-		log.error("Unknown Intent Name: " + intentName);
+		System.out.println("Unknown Intent Name: " + intentName);
 		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
 		outputSpeech.setText("Sanjoy has no idea what to do with this intent: " + intentName);
 		return SpeechletResponse.newTellResponse(outputSpeech);
@@ -93,6 +87,6 @@ public class EarningsSpeechlet implements Speechlet  {
 	
 	@Override
 	public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
-		// TODO Auto-generated method stub	
+    	System.out.println("onSessionEnded: " + session.getSessionId() + "  " + session.getAttributes().size());
 	}
 }
