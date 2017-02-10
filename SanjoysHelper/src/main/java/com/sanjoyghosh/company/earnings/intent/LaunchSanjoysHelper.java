76abd3dd6b54595a28c7ab53f7c3a45904f11dc2
Sanjoy Ghosh <sghosh@hytrust.com>
2017-02-10 10:34:57 -0800

    Fixes 2/10/2017

---
+++ SanjoysHelper/src/main/java/com/sanjoyghosh/company/earnings/intent/LaunchSanjoysHelper.java
@@ -0,0 +1,24 @@
+package com.sanjoyghosh.company.earnings.intent;
+
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+
+public class LaunchSanjoysHelper implements InterfaceIntent {
+
+	@Override
+	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
+		String text = "Hello, Sanjoy's Helper is always open to help you. " +
+			"You can ask her for realtime quotes on listed US stocks. " +
+			"You can identify the company by its name, or ticker symbol. " +
+			"You can spell the symbol either by letter or using the Wikipedia Spelling Alphabet. " +
+			"Just use England for E when using the Spelling Alphabet. " +
+			"For example, Alexa, ask Sanjoy's Helper the price of amazon. ";
+		
+		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+		outputSpeech.setText(text);
+		return SpeechletResponse.newTellResponse(outputSpeech);
+	}
+}
