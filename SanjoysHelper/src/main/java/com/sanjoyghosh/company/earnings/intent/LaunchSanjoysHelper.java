4de73a9dc157e1e3f453963bbd14cadeff0cf9f2
Sanjoy Ghosh <sghosh@hytrust.com>
2017-02-15 10:56:30 -0800

    Fixes 2/15/2017

--- SanjoysHelper/src/main/java/com/sanjoyghosh/company/earnings/intent/LaunchSanjoysHelper.java
+++ SanjoysHelper/src/main/java/com/sanjoyghosh/company/earnings/intent/LaunchSanjoysHelper.java
@@ -4,19 +4,27 @@ import com.amazon.speech.speechlet.Session;
 import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
 
 public class LaunchSanjoysHelper {
 
 	public static SpeechletResponse onLaunch(Session session) throws SpeechletException {
 		String text = "Hello, Sanjoy's Helper is always open to help you. " +
-			"You can ask her for realtime quotes on listed US stocks. " +
+			"You can ask her for roughly realtime quotes on listed US stocks. " +
 			"You can identify the company by its name, or ticker symbol. " +
 			"You can spell the symbol either by letter or using the Wikipedia Spelling Alphabet. " +
 			"Just use England for E when using the Spelling Alphabet. " +
-			"For example, Alexa, ask Sanjoy's Helper, the price of amazon. ";
+			"For example, Alexa, ask Sanjoy's Helper, the price of amazon. " +
+			"Go ahead, ask the price of your favorite stock.";
 		
 		PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
 		outputSpeech.setText(text);
-		return SpeechletResponse.newTellResponse(outputSpeech);
+		
+		Reprompt reprompt = new Reprompt();
+		PlainTextOutputSpeech repromptSpeech = new PlainTextOutputSpeech();
+		repromptSpeech.setText("Sorry, ask the price of a stock, or say Stop or Cancel or Exit.");
+		reprompt.setOutputSpeech(repromptSpeech);
+
+		return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
 	}
 }
