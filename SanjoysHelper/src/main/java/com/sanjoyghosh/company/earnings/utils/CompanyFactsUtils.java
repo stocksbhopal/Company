0f068411d9db60ff7428eccd62786ea9a206b4a5
Sanjoy Ghosh <sanjoyg@lab126.com>
2017-02-24 15:23:09 -0800

    Fixes 2/24/2017

--- SanjoysHelper/src/main/java/com/sanjoyghosh/company/earnings/utils/CompanyFactsUtils.java
+++ SanjoysHelper/src/main/java/com/sanjoyghosh/company/earnings/utils/CompanyFactsUtils.java
@@ -70,6 +70,7 @@ public class CompanyFactsUtils {
 		companyFactsByNameMap.put("synopsis", companyFactsBySymbolMap.get("SNPS"));
 		companyFactsByNameMap.put("AT and T", companyFactsBySymbolMap.get("T"));
 		companyFactsByNameMap.put("tea", companyFactsBySymbolMap.get("T"));
+		companyFactsByNameMap.put("footlocker", companyFactsBySymbolMap.get("FL"));
 	}
 	
 
