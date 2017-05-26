d70fcf14043a3e0b52532bf8845aa565c18ae689
Sanjoy Ghosh <sghosh@hytrust.com>
2017-05-25 20:51:21 -0700

    Fixes Old

--- Company/src/main/java/com/sanjoyghosh/company/source/reuters/ReutersMorningNewsCallReader.java
+++ Company/src/main/java/com/sanjoyghosh/company/source/reuters/ReutersMorningNewsCallReader.java
@@ -1,5 +1,6 @@
 package com.sanjoyghosh.company.source.reuters;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.LineNumberReader;
---
+++ FinanceHelper/src/main/java/com/example/demo/FinanceHelperApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceHelperApplication.class, args);
+	}
+}
