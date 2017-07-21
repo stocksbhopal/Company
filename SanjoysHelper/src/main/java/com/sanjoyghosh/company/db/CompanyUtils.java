da50a3fdc6ebe59ae219e19a761d47382327a978
Sanjoy Ghosh <sghosh@hytrust.com>
2017-07-20 21:50:42 -0700

    Fixes 07/20/2017

--- SanjoysHelper/src/main/java/com/sanjoyghosh/company/db/CompanyUtils.java
+++ SanjoysHelper/src/main/java/com/sanjoyghosh/company/db/CompanyUtils.java
@@ -2,23 +2,16 @@ package com.sanjoyghosh.company.db;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 
 import com.sanjoyghosh.company.api.CompanyEarnings;
 import com.sanjoyghosh.company.api.MarketIndexEnum;
-import com.sanjoyghosh.company.db.model.Activity;
 import com.sanjoyghosh.company.db.model.Company;
 import com.sanjoyghosh.company.db.model.EarningsDate;
-import com.sanjoyghosh.company.db.model.Holding;
-import com.sanjoyghosh.company.db.model.PriceHistory;
-import com.sanjoyghosh.company.db.model.StockSplitHistory;
 
 public class CompanyUtils {
 
@@ -72,180 +65,9 @@ public class CompanyUtils {
 		}
 		return companyByIdMap;
 	}
-
 	
-	
-	
-	/**
-	 * For Merrill Lynch, settledDate is Non Null.
-	 */
-	public static Set<Activity> fetchAllActivityAtBrokerageForSettledDate(EntityManager entityManager, String brokerage, LocalDate settledDate) {
-		List<Activity> activityList = 
-			entityManager.createQuery("SELECT a FROM Activity AS a WHERE a.brokerage = :brokerage AND a.settledDate = :settledDate", Activity.class)
-			.setParameter("brokerage", brokerage)
-			.setParameter("settledDate", settledDate)
-			.getResultList();
-		Set<Activity> activitySet = new HashSet<Activity>();
-		activitySet.addAll(activityList);
-		return activitySet;
-	}
-
-	/**
-	 * For Fidelity, tradeDate is Non Null.
-	 */
-	public static Set<Activity> fetchAllActivityAtBrokerageForTradeDate(EntityManager entityManager, String brokerage, Timestamp tradeDate) {
-		List<Activity> activityList = 
-			entityManager.createQuery("SELECT a FROM Activity AS a WHERE a.brokerage = :brokerage AND a.tradeDate = :tradeDate", Activity.class)
-			.setParameter("brokerage", brokerage)
-			.setParameter("tradeDate", tradeDate)
-			.getResultList();
-		Set<Activity> activitySet = new HashSet<Activity>();
-		activitySet.addAll(activityList);
-		return activitySet;
-	}
-	
-	
-	
-	public static List<Holding> fetchAllHoldingsAtBrokerage(EntityManager entityManager, String brokerage) {
-		List<Holding> holdingList = 
-			entityManager.createQuery("SELECT h FROM Holding AS h WHERE h.brokerage = :brokerage ORDER BY h.symbol ASC", Holding.class)
-			.setParameter("brokerage", brokerage)
-			.getResultList();
-		return holdingList;
-	}
-
-	public static List<String> fetchAllHoldingsSymbols(EntityManager entityManager) {
-		List<String> symbols = 
-			entityManager.createQuery("SELECT h.symbol FROM Holding AS h ORDER BY h.symbol ASC", String.class)
-			.getResultList();
-		return symbols;
-	}
-
-	public static int deleteAllHoldingsByDateBrokerage(EntityManager entityManager, String brokerage, Date cobDate) {
-		int numHoldingsDeleted = 
-			entityManager.createQuery("DELETE FROM Holding AS h WHERE h.brokerage = :brokerage AND h.cobDate = :cobDate")
-			.setParameter("brokerage", brokerage)
-			.setParameter("cobDate", cobDate)
-			.executeUpdate();
-		return numHoldingsDeleted;
-	}
-
-
-
-
-
-	
-	public static StockSplitHistory fetchStockSplitHistoryForSymbolDate(EntityManager entityManager, String symbol, Timestamp stockSplitDate) {
-		List<StockSplitHistory> stockSplitHistoryList = 
-			entityManager.createQuery("SELECT sh FROM StockSplitHistory AS sh WHERE sh.symbol = :symbol AND sh.dateOfSplit = :stockSplitDate", StockSplitHistory.class)
-			.setParameter("symbol", symbol)
-			.setParameter("stockSplitDate", stockSplitDate)
-			.getResultList();
-		return stockSplitHistoryList == null || stockSplitHistoryList.size() == 0 ? null : stockSplitHistoryList.get(0);
-	}
-
-	public static List<StockSplitHistory> fetchStockSplitHistoryForSymbol(EntityManager entityManager, String symbol) {
-		List<StockSplitHistory> stockSplitHistoryList = 
-			entityManager.createQuery("SELECT sh FROM StockSplitHistory AS sh WHERE sh.symbol = :symbol ORDER BY sh.dateOfSplit DESC", StockSplitHistory.class)
-			.setParameter("symbol", symbol)
-			.getResultList();
-		return stockSplitHistoryList;
-	}
 	
 
-
-	
-	public static PriceHistory fetchLastPriceHistoryForSymbol(EntityManager entityManager, String symbol) {
-		List<PriceHistory> priceHistoryList = 
-			entityManager.createQuery("SELECT ph FROM PriceHistory AS ph WHERE ph.symbol = :symbol AND ph.dateOfPrice IN (SELECT MAX(dateOfPrice) FROM PriceHistory WHERE symbol = :symbol)", PriceHistory.class)
-			.setParameter("symbol", symbol)
-			.getResultList();
-		return priceHistoryList == null || priceHistoryList.size() == 0 ? null : priceHistoryList.get(0);
-	}
-	
-	public static List<PriceHistory> fetchPriceHistoryForSymbolDateStartEnd(EntityManager entityManager, String symbol, Timestamp priceDateStart, Timestamp priceDateEnd) {
-		List<PriceHistory> priceHistoryList = 
-			entityManager.createQuery("SELECT ph FROM PriceHistory AS ph WHERE ph.symbol = :symbol AND ph.dateOfPrice >= :priceDateStart AND ph.dateOfPrice <= :priceDateEnd", PriceHistory.class)
-			.setParameter("symbol", symbol)
-			.setParameter("priceDateStart", priceDateStart)
-			.setParameter("priceDateEnd", priceDateEnd)
-			.getResultList();
-		return priceHistoryList;
-	}
-
-	
-	/*
-	public static List<CompanyPrice> fetchCompanyPriceListForAlexaUserLimit(EntityManager entityManager, String alexaUser, int limit, boolean gainers) {
-		List<CompanyPrice> companyPriceList = entityManager.createQuery(
-			"SELECT new com.sanjoyghosh.company.api.CompanyPrice(c.symbol, c.name, p.price, p.priceChange, p.priceChangePercent) " +
-				"FROM MyStocks AS m, Company AS c, Price AS p, AlexaUser AS a " +
-				"WHERE a.alexaUser = :alexaUser AND a.id = m.alexaUserId AND m.companyId = c.id AND c.id = p.companyId " +
-				"ORDER BY p.priceChangePercent " + (gainers ? "DESC" : "ASC"), CompanyPrice.class)
-				.setParameter("alexaUser", alexaUser)
-				.setMaxResults(limit)
-				.getResultList();
-		return companyPriceList;
-	}
-
-	public static List<CompanyPrice> fetchCompanyPriceListForAlexaUser(EntityManager entityManager, String alexaUser) {
-		List<CompanyPrice> companyPriceList = entityManager.createQuery(
-			"SELECT new com.sanjoyghosh.company.api.CompanyPrice(c.symbol, c.name, p.price, p.priceChange, p.priceChangePercent) " +
-				"FROM MyStocks AS m, Company AS c, Price AS p, AlexaUser AS a " +
-				"WHERE a.alexaUser = :alexaUser AND a.id = m.alexaUserId AND m.companyId = c.id AND c.id = p.companyId " +
-				"ORDER BY p.priceChangePercent DESC", CompanyPrice.class)
-				.setParameter("alexaUser", alexaUser)
-				.getResultList();
-		return companyPriceList;
-	}
-	
-	
-	public static List<Price> fetchPriceList(EntityManager entityManager) {
-		List<Price> priceList = entityManager.createQuery("SELECT p FROM Price AS p", Price.class).getResultList();
-		return priceList;
-	}
-	
-	
-	
-	public static List<Company> fetchCompanyListForAllUsersMinusPrice(EntityManager entityManager) {
-		List<Company> companyList =
-			entityManager.createQuery(
-				"SELECT new com.sanjoyghosh.company.db.model.Company(c.id, c.symbol) FROM MyStocks AS m, Company AS c WHERE m.companyId = c.id AND " +
-					"m.companyId IN (SELECT DISTINCT(companyId) FROM MyStocks) AND " +
-					"m.companyId NOT IN (SELECT companyId FROM Price)", Company.class)
-			.getResultList();
-		return companyList;
-	}
-
-	public static List<Company> fetchCompanyListForAlexaUserId(EntityManager entityManager, int alexaUserId) {
-		List<Company> companyList =
-			entityManager.createQuery("SELECT new com.sanjoyghosh.company.db.model.Company(c.id, c.symbol) FROM MyStocks AS m, Company AS c WHERE m.companyId = c.id AND alexaUserId = :alexaUserId", Company.class)
-			.setParameter("alexaUserId", alexaUserId)
-			.getResultList();
-		return companyList;
-	}
-	
-	
-	public static List<Portfolio> fetchMyStocksListForAlexaUser(EntityManager entityManager, String alexaUser) {
-		List<Portfolio> myStocksList =
-			entityManager.createQuery("SELECT m FROM MyStocks AS m, AlexaUser AS a WHERE m.alexaUserId = a.id AND a.alexaUser = :alexaUser", Portfolio.class)
-			.setParameter("alexaUser", alexaUser)
-			.getResultList();
-		return myStocksList;		
-	}
-	
-	public static Map<Integer, Portfolio> fetchMyStocksMapByCompanyIdForAlexaUser(EntityManager entityManager, String alexaUser) {
-		List<Portfolio> myStocksList =
-			entityManager.createQuery("SELECT m FROM MyStocks AS m, AlexaUser AS a WHERE m.alexaUserId = a.id AND a.alexaUser = :alexaUser", Portfolio.class)
-			.setParameter("alexaUser", alexaUser)
-			.getResultList();
-		
-		Map<Integer, Portfolio> myStocksByCompanyIdMap = new HashMap<>();
-		for (Portfolio myStocks : myStocksList) {
-			myStocksByCompanyIdMap.put(myStocks.getCompanyId(), myStocks);
-		}
-		return myStocksByCompanyIdMap;		
-	}
-	*/
 	
 	
 	private static String marketIndexToColumn(MarketIndexEnum index) {
--- SanjoysHelper/src/main/java/com/sanjoyghosh/company/source/nasdaq/NasdaqRealtimeQuote.java
+++ SanjoysHelper/src/main/java/com/sanjoyghosh/company/source/nasdaq/NasdaqRealtimeQuote.java
@@ -6,12 +6,6 @@ public class NasdaqRealtimeQuote {
 	private Double	price;
 	private Double	priceChange;
 	private Double	priceChangePercent;
-	private Double	previousClose;
-	private Integer	volumeToday;
-	private Double	lowToday;
-	private Double	highToday;
-	private Double	low52Week;
-	private Double	high52Week;
 	
 	
 	public Double getPrice() {
@@ -38,61 +32,6 @@ public class NasdaqRealtimeQuote {
 	}
 	
 	
-	public Double getPreviousClose() {
-		return previousClose;
-	}
-	public void setPreviousClose(Double previousClose) {
-		this.previousClose = previousClose;
-	}
-	
-	
-	public Integer getVolumeToday() {
-		return volumeToday;
-	}
-	public void setVolumeToday(Integer volumeToday) {
-		this.volumeToday = volumeToday;
-	}
-	
-	
-	public Double getLowToday() {
-		return lowToday;
-	}
-	public void setLowToday(Double lowToday) {
-		this.lowToday = lowToday;
-	}
-	
-	
-	public Double getHighToday() {
-		return highToday;
-	}
-	public void setHighToday(Double highToday) {
-		this.highToday = highToday;
-	}
-	
-	
-	public Double getLow52Week() {
-		return low52Week;
-	}
-	public void setLow52Week(Double low52Week) {
-		this.low52Week = low52Week;
-	}
-	
-	
-	public Double getHigh52Week() {
-		return high52Week;
-	}
-	public void setHigh52Week(Double high52Week) {
-		this.high52Week = high52Week;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "NasdaqRealtimeQuote [price=" + price + ", priceChange=" + priceChange + ", priceChangePercent="
-				+ priceChangePercent + ", previousClose=" + previousClose + ", volumeToday=" + volumeToday
-				+ ", lowToday=" + lowToday + ", highToday=" + highToday + ", low52Week=" + low52Week + ", high52Week="
-				+ high52Week + "]";
-	}
 	public String getSymbol() {
 		return symbol;
 	}
