3b63a60e0a74b2fd788fd07cb4bd1265fc0c0419
Sanjoy Ghosh <stocksbhopal@gmail.com>
2017-11-08 11:17:28 -0800

    Checkin CompanyDynamoDB

---
+++ CompanyDynamoDB/src/main/java/com/sanjoyghosh/company/dynamodb/App.java
@@ -0,0 +1,13 @@
+package com.sanjoyghosh.company.dynamodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
