0cf0a77767114c2d820ffa7a83419f7b4fc26079
Sanjoy Ghosh <sghosh@hytrust.com>
2017-11-19 08:50:56 -0800

    Fixes 11/19/2017

---
+++ CompanyAlexaSkill/src/main/java/com/sanjoyghosh/company/alexaskill/servlet/EarningsServlet.java
@@ -0,0 +1,19 @@
+package com.sanjoyghosh.company.alexaskill.servlet;
+
+import javax.servlet.ServletException;
+
+import com.amazon.speech.speechlet.servlet.SpeechletServlet;
+
+public class EarningsServlet extends SpeechletServlet {
+
+	private static final long serialVersionUID = 8530879718768516833L;
+
+	public EarningsServlet() {
+		this.setSpeechlet(new EarningsSpeechlet());
+	}
+
+	@Override
+	public void init() throws ServletException {
+		EarningsSpeechlet.init();
+	}
+}
---
+++ CompanyAlexaSkill/src/main/java/com/sanjoyghosh/company/alexaskill/servlet/EarningsSpeechletRequestStreamHandler.java
@@ -0,0 +1,25 @@
+package com.sanjoyghosh.company.alexaskill.servlet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.BasicConfigurator;
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+public class EarningsSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+
+    private static final Set<String> supportedApplicationIds = new HashSet<String>();
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
+         * Alexa Skill and put the relevant Application Ids in this Set.
+         */
+        supportedApplicationIds.add("amzn1.ask.skill.8321fccf-fece-461b-bb3a-51d61aa139a9");
+    }
+
+    public EarningsSpeechletRequestStreamHandler() {
+        super(new EarningsSpeechlet(), supportedApplicationIds);
+        BasicConfigurator.configure();
+    }
+}
